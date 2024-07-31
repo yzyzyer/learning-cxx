1d8fda4862d38ef9101bc00ab751512be3eff96b
yzyzyer <sunjiaxi01@163.com>
2024-07-31 19:56:09 +0800

    update learn 4

--- exercises/01_variable&add/main.cpp
+++ exercises/01_variable&add/main.cpp
@@ -1,3 +1,4 @@
+#include<iostream>
 #include "../exercise.h"
 
 // READ: 运算符 <https://zh.cppreference.com/w/cpp/language/expressions#.E8.BF.90.E7.AE.97.E7.AC.A6>
@@ -5,6 +6,8 @@
 int main(int argc, char **argv) {
     // TODO: 补全变量定义并打印加法运算
     // x ?
+    
+    int x = 5;
     std::cout << x << " + " << x << " = " << x + x << std::endl;
     return 0;
 }
