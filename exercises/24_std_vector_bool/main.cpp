f4d6d712fcde824bb772d0d50ec270dcf3814373
yzyzyer <sunjiaxi01@163.com>
2024-08-04 23:16:28 +0800

    xmake problems

--- exercises/24_std_vector_bool/main.cpp
+++ exercises/24_std_vector_bool/main.cpp
@@ -11,7 +11,7 @@ int main(int argc, char **argv) {
     ASSERT(vec[99], "Make this assertion pass.");
     ASSERT(vec.size() == 100, "Make this assertion pass.");
     // std::cout<<sizeof(vec)<<std::endl;
-    ASSERT(sizeof(vec) == 32, "Fill in the correct value.");
+    ASSERT(sizeof(vec) == 40, "Fill in the correct value.");
     {
         vec[20] = false;
         ASSERT(!vec[20], "Fill in `vec[20]` or `!vec[20]`.");
